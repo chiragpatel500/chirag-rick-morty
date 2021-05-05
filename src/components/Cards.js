dc91e24e30be47dfdc2644194ecd1f620cca8c6d
chiragpatel500 <chirag12354@gmail.com>
2021-05-05 14:50:45 +0200

    update

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -13,13 +13,12 @@ function Cards() {
             .then((response) => response.json())
             .then((data) => setCharacters(data.results));
     };
-     
-  const handleSearchInputChange = (event) => {
-    setSearchText(event.target.value);
-  };
-  const filteredCharacter = characters.filter((character) => {
-    return character.name.toUpperCase().includes(searchText.toUpperCase());
-  });
+    const handleSearchInputChange = (event) => {
+     setSearchText(event.target.value);
+    };
+    const filteredCharacter = characters.filter((character) => {
+        return character.name.toUpperCase().includes(searchText.toUpperCase());
+    });
     
     useEffect(() => {
         fetchApi();
@@ -74,3 +73,28 @@ function Cards() {
 };
 
 export default Cards;
+
+// Demo Modal
+// <!-- Button trigger modal -->
+// <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+//   Launch demo modal
+// </button>
+
+// <!-- Modal -->
+// <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+//   <div class="modal-dialog">
+//     <div class="modal-content">
+//       <div class="modal-header">
+//         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+//         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+//       </div>
+//       <div class="modal-body">
+//         ...
+//       </div>
+//       <div class="modal-footer">
+//         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+//         <button type="button" class="btn btn-primary">Save changes</button>
+//       </div>
+//     </div>
+//   </div>
+// </div>
